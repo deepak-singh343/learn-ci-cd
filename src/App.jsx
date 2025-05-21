4bd3f635874c4356ea9e57d096e0f2631aaafb46
deepak-singh343 <deepak.negi343@gmail.com>
2025-05-21 21:19:37 +0530

    initalized project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <>Learning AWS and CI/CD</>;
+}
+
+export default App;
